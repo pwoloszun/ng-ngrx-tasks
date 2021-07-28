4afdbaca1cab42253ea7b0d5f799a14bf247c5dc
pwoloszun <piotr.woloszun@gmail.com>
2021-07-28 11:11:09 +0200

    todos p1

--- src/app/features/04-todos/pages/todos/todos.component.ts
+++ src/app/features/04-todos/pages/todos/todos.component.ts
@@ -4,6 +4,8 @@ import { TodoFormVm } from '../../components/todos-form/todos-form.component';
 import { TodoModel } from '../../models/todo.model';
 import { TODOS_DATA } from '../../fake-data/todos-data';
 
+// Container / Smart Component
+// "MVC Controller"
 @Component({
   selector: 'nts-todos',
   templateUrl: './todos.component.html',
@@ -16,11 +18,15 @@ export class TodosComponent implements OnInit {
     this.todos = TODOS_DATA;
   }
 
-  deleteTodo(todo) {
+  deleteTodo(todo: TodoModel) {
+    console.log('del', todo);
+
     // TODO
   }
 
   createTodo(todoData: TodoFormVm) {
+    console.log('creat', todoData);
+
     // TODO
   }
 }
