8440a0a19f767ad252891a3cd665e06041e9bef4
pwoloszun <piotr.woloszun@gmail.com>
2022-01-19 15:27:50 +0100

    state shape design

--- docs/tasks/design-state-shape-todos.ts
+++ docs/tasks/design-state-shape-todos.ts
@@ -5,7 +5,6 @@ interface Todo {
 }
 
 enum TodoStatus {
-  SAVING = 'SAVING',
   IS_EDITING = 'IS_EDITING',
   IS_SAVING = 'IS_SAVING',
   IS_REMOVING = 'IS_REMOVING',
@@ -13,15 +12,48 @@ enum TodoStatus {
 }
 
 // data fetched from API
-const todosData = [
-  { id: 100, title: 'buy milk', isImportant: true },
+const todosData: Todo[] = [
+  { id: 100, title: 'buy milk', },
   { id: 200, title: 'walk the dog', description: 'important' },
   { id: 300, title: 'do them math', },
 ];
 
 
 // TODO: designing state shape
+// snapshot
 const stateSlice = {
+
+  // persisted/entities state
+  todos: [
+    { id: 100, title: 'buy milk', },
+    { id: 200, title: 'walk the dog', description: 'important', },
+    { id: 300, title: 'do them math', },
+  ],
+
+  // app/feature state
+  todoStatuses: {
+    100: TodoStatus.IS_EDITING,
+    200: TodoStatus.PERSISTED,
+    300: TodoStatus.IS_REMOVING,
+  },
+
+  isImportant: {
+    100: false
+  },
+
+  customOrder: [200, 100, 300],
+
+  selectedSort: { sortBy: 'title', asc: true },
+
+};
+
+const selectAllTodosWithStatuses = (stateSlice) => {
+  return stateSlice.todos.map((todo) => {
+    return {
+      ...todo,
+      status: stateSlice.todoStatuses[todo.id]
+    };
+  });
 };
 
 // ======
